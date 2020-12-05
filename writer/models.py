e8f2f8bd96850808463323553d3aaac207fb740f
Pedro Fonseca <peidrao>
2020-12-04 23:02:56 -0300

    nova aplicaçao

--- writer/models.py
+++ writer/models.py
@@ -1,3 +1,31 @@
+from django.utils.safestring import mark_safe
+from django.urls import reverse
+
 from django.db import models
+from django.template.defaultfilters import slugify
+from ckeditor_uploader.fields import RichTextUploadingField
 
 # Create your models here.
+
+
+class Writer(models.Model):
+    fullname = models.CharField(max_length=100, null=False)
+    description = RichTextUploadingField()
+    slug = models.SlugField(unique=True, null=False, blank=True)
+    image = models.FileField(upload_to='images/', null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.fullname
+
+    def image_tag(self):
+        if self.image.url is not None:
+            return mark_safe('<img src={} height="50" />'.format(self.image.url))
+        else:
+            return ""
+
+    def get_absolute_url(self):
+        return reverse("writer_detail", kwargs={"slug": self.slug})
+

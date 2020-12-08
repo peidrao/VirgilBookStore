f1e1a1b8d46bea56c3d7165ebaff34884cfcc6bc
Pedro Fonseca <peidrao>
2020-12-07 22:30:20 -0300

    problema com zoom

--- Store/urls.py
+++ Store/urls.py
@@ -4,11 +4,14 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from home import views
+#from order import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('home.urls')),
     path('home/', include('home.urls')),
+
+    path('book/<int:id>/<slug:slug>', views.book_detail, name='book_detail')
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from book.models import Book
+from book.models import Book, Genre, Images
 
 
 def index(request):
@@ -10,3 +10,18 @@ def index(request):
         'books_latest': books_latest
     }
     return render(request, 'index.html', context)
+
+
+def book_detail(request, id, slug):
+    query = request.GET.get('q')
+    genre = Genre.objects.all()
+    book = Book.objects.get(pk=id)
+
+    images = Images.objects.filter(book_id=id)
+    context = {
+        'genre': genre,
+        'book': book,
+        'images': images,
+    }
+
+    return render(request, 'pages/book_detail.html', context)

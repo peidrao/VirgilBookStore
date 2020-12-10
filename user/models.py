e745cfacd07919924154868b31f5fe866daf095c
Pedro Fonseca <peidrao>
2020-12-10 19:42:10 -0300

    formulario  ok

--- user/models.py
+++ user/models.py
@@ -11,19 +11,17 @@ class UserProfile(models.Model):
     phone = models.CharField(max_length=15, blank=True)
     zip_code = models.CharField(max_length=10, blank=True)
     address = models.CharField(max_length=150, blank=True)
-    number_address = models.IntegerField(blank=True)
+    #number_address = models.IntegerField(blank=True)
     city = models.CharField(max_length=50, blank=True)
     state = models.CharField(max_length=50, blank=True)
     image = models.FileField(upload_to='user/', blank=True)
 
     def __str__(self):
-        return self.username
+        return self.user.username
 
     def user_name(self):
-        return self.user.first_name + ' ' + self.user.last_name
+        return self.user.first_name + ' ' + self.user.last_name + ' [' + self.user.username + '] '
 
     def image_tag(self):
-        if self.image.url is not None:
-            return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
-        else:
-            return ""
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.contrib import messages
 
 from book.models import Genre
 from .forms import SignUpForm
@@ -15,19 +16,27 @@ def signup_form(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
+
             form.save()
+
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
+
             user = authenticate(username=username, password=password)
+
             login(request, user)
 
             current_user = request.user
+
             data = UserProfile()
+
             data.user_id = current_user.id
             data.image = 'images/users/user.png'
             data.save()
+            messages.success(request, 'Your account has been created!')
             return HttpResponseRedirect('/')
         else:
+            messages.warning(request, form.errors)
             return HttpResponseRedirect('/signup')
 
     form = SignUpForm()

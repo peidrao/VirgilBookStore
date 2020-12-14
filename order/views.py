4ce8800fac45e85337dfb0a89e6f181ee74e0324
Pedro Fonseca <peidrao>
2020-12-14 18:45:38 -0300

    Iniciando modulo de compra

--- order/views.py
+++ order/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+from .models import ShopCart
+
+
+def index(request):
+    return HttpResponse('Teste')
+
+
+@login_required(login_url='/login')
+def add_shop_cart(request, id):
+    url = request.META.get('HTTP_REFERER')
+    current_user = request.user
+
+    check_product = ShopCart.objects.filter(product_id=id)
+
+    if check_product:
+        control = 1
+    else:
+        control = 0
+
+    if request.method == 'POST':
+        form  = ShopCart(request.POST)
+        if form.i

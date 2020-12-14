ab5397abc474674c71064ddfd76a1a49531886bb
Pedro Fonseca <peidrao>
2020-12-14 19:34:40 -0300

    tabela de comprar feitas pelo usuario

--- order/urls.py
+++ order/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index').
+    path('', views.index, name='index'),
+    path('addtoshopcart/<int:id>', views.add_shop_cart, name='add_shop_cart'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 # Create your views here.
 from .models import ShopCart
+from .forms import ShopCartForm
 
 
 def index(request):
@@ -13,13 +15,39 @@ def add_shop_cart(request, id):
     url = request.META.get('HTTP_REFERER')
     current_user = request.user
 
-    check_product = ShopCart.objects.filter(product_id=id)
+    check_book = ShopCart.objects.filter(book_id=id)
 
-    if check_product:
+    if check_book:
         control = 1
     else:
         control = 0
 
     if request.method == 'POST':
-        form  = ShopCart(request.POST)
-        if form.i
+        form = ShopCartForm(request.POST)
+        if form.is_valid():
+            if control == 1:
+                data = ShopCart.objects.get(book_id=id)
+                data.quantity += form.cleaned_data['quantity']
+                data.save()
+            else:
+                data = ShopCart()
+                data.user_id = current_user.id
+                data.book_id = id
+                data.quantity = form.cleaned_data['quantity']
+                data.save()
+            messages.success(request, "Livro adicionando no carrinho")
+        return HttpResponseRedirect(url)
+
+
+def shopcart(request):
+    category = Category.objects.all()
+    current_user = request.user
+    shopcart = ShopCart.objects.filter(user_id=current_user.id)
+    total = 0
+    for book in shopcart:
+        total += book.book.price * book.quantity
+
+    context = {
+        'category': category, 'shopcart': shopcart, 'total': total}
+
+    return render(request, 'shopcart_books.html', context)

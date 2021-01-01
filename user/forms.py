5a840f8ea8d347c0dda8ff59f2393a2d7e7dc3ea
Pedro Victor da Fonseca <peidrao>
2021-01-01 19:35:23 -0300

    arrumando formulario de criaçao de conta

--- user/forms.py
+++ user/forms.py
@@ -6,15 +6,8 @@ from user.models import UserProfile
 
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=30, label='User Name :')
-    email = forms.EmailField(max_length=200, label='Email :')
-    first_name = forms.CharField(
-        max_length=100, help_text='First Name', label='First Name :')
-    last_name = forms.CharField(
-        max_length=100, help_text='Last Name', label='First Name :')
 
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name',
                   'last_name', 'password1', 'password2', )
-

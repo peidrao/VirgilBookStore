cd413b98132796a110818f524519066475022fba
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-03 20:10:17 -0300

    update: str

--- book/models.py
+++ book/models.py
@@ -30,7 +30,7 @@ class Genre(MPTTModel):
         order_insertion_by = ['title']
 
     def __str__(self):
-        return self.title
+        return str(self.title)
 
     def get_absolute_url(self):
         return reverse("genre_detail", kwargs={"slug": self.slug})

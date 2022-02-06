14ad1d326b688111d766ff2d067cbcf725ecaab7
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-06 10:29:43 -0300

    add: templates folder

--- virgilbookstore/settings.py
+++ virgilbookstore/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,7 +51,7 @@ ROOT_URLCONF = 'virgilbookstore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates", os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

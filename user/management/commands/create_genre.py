5a93c70930decbea6caa13399e4317f8f7bdea84
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-16 00:29:40 -0300

    update commands

--- user/management/commands/create_genre.py
+++ user/management/commands/create_genre.py
@@ -8,14 +8,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if not Genre.objects.filter(slug="arte").exists():
             Genre.objects.create(title="Arte")
-        if not Genre.objects.filter(slug="acao").exists():
-            Genre.objects.create(title="Ação")
         if not Genre.objects.filter(slug="comedia").exists():
             Genre.objects.create(title="Comédia")
+        if not Genre.objects.filter(slug="matematica").exists():   
+            Genre.objects.create(title="Matemática")
         if not Genre.objects.filter(slug="ficcao").exists():
             Genre.objects.create(title="Ficção")
-        if not Genre.objects.filter(slug="contos-de-fada").exists():
-            Genre.objects.create(title="Contos de Fada")
         if not Genre.objects.filter(slug="poesia").exists():
             Genre.objects.create(title="Poesia")
         if not Genre.objects.filter(slug="politica").exists():
@@ -25,4 +23,17 @@ class Command(BaseCommand):
         if not Genre.objects.filter(slug="economia").exists():
             Genre.objects.create(title="Econômia")
         if not Genre.objects.filter(slug="filosofia").exists():
-            Genre.objects.create(title="Filosofia")
+            philosophy = Genre.objects.create(title="Filosofia")
+            if not Genre.objects.filter(slug='filosofia-medieval').exists():
+                Genre.objects.create(title="Filosofia Medieval", origin=philosophy)
+            if not Genre.objects.filter(slug='filosofia-estoica').exists():
+                Genre.objects.create(title="Filosofia Estóica", origin=philosophy)
+        if not Genre.objects.filter(slug='historia').exists():
+            history = Genre.objects.create(title="História")
+            if not Genre.objects.filter(slug='historia-do-brasil').exists():
+                Genre.objects.create(title="História do Brasil", origin=history)
+            if not Genre.objects.filter(slug='historia-do-0ocidente').exists():
+                Genre.objects.create(title="História do Ocidente", origin=history)
+            if not Genre.objects.filter(slug='historia-africana').exists():
+                Genre.objects.create(title="História Africana", origin=history)
+            

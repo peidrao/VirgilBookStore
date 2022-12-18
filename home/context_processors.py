a9d07ddf30a30ba396398230a0bd2b3c426ed23a
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-17 21:45:31 -0300

    add context - categories with navbar

--- home/context_processors.py
+++ home/context_processors.py
@@ -2,5 +2,5 @@ from book.models import Genre
 
 
 def menu_genres(request):
-    genres = Genre.objects.all()
+    genres = Genre.objects.filter(origin__isnull=True).order_by('title')
     return {'genres': genres}

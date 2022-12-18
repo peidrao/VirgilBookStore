d5b523494f998b6fc92349702ba097fdf706029d
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-17 21:55:28 -0300

    update models

--- book/models.py
+++ book/models.py
@@ -5,6 +5,7 @@ from django.db.models import Avg, Count
 from django.urls import reverse
 from django.db import models
 from user.models import Profile
+from django.utils.translation import gettext as _
 
 
 def slugify_pre_save(sender, instance, *args, **kwargs):
@@ -37,8 +38,8 @@ class Genre(models.Model):
 class Writer(models.Model):
     fullname = models.CharField(max_length=100, null=False)
     description = models.TextField(null=True, blank=True)
-    slug = models.SlugField(unique=True, null=False, blank=True)
-    image = models.FileField(upload_to='images/writer', null=False)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    image = models.FileField(upload_to='images/writer', null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -57,27 +58,26 @@ class Writer(models.Model):
 
 
 class Book(models.Model):
-    STATUS = (
-        ('Sim', 'True'),
-        ('Não', 'False'),
-    )
+    class StatusChoice(models.IntegerChoices):
+        YES = 1, _('Yes')
+        NOT = 2, _('Not')
 
-    writer = models.ForeignKey(Writer, on_delete=models.CASCADE, null=False)
+    writer = models.ForeignKey(Writer, on_delete=models.SET_NULL, null=True)
     genre = models.ForeignKey(
-        Genre, on_delete=models.CASCADE, null=False)
+        Genre, on_delete=models.SET_NULL, null=True)
 
     title = models.CharField(max_length=150, null=False)
     description = models.TextField(null=True, blank=True)
     keywords = models.CharField(max_length=255, null=True, blank=True)
     publication_date = models.DateField(auto_now_add=True)
-    image = models.FileField(upload_to='images/capa', null=False)
+    image = models.FileField(upload_to='images/capa', null=True)
 
     price = models.FloatField()
     amount = models.IntegerField()
     specification = models.TextField(null=True, blank=True)
-    slug = models.SlugField(unique=True, null=False)
+    slug = models.SlugField(unique=True, null=True)
 
-    status = models.CharField(choices=STATUS, max_length=5)
+    status = models.IntegerField(choices=StatusChoice.choices, default=StatusChoice.YES)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -112,7 +112,7 @@ class Book(models.Model):
 
 
 class Images(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=50, blank=True)
     image = models.FileField(blank=True, upload_to='images/books')
 
@@ -121,20 +121,19 @@ class Images(models.Model):
 
 
 class Comment(models.Model):
-    STATUS = (
-        ('Nova', 'Nova'),
-        ('Verdade', 'Verdade'),
-        ('Falso', 'Falso'),
-    )
+    class StatusChoice(models.IntegerChoices):
+        NEW = 1, _('New')
+        READ = 2, _('Read')
+        TRASH = 3, _('Trash')
 
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     subject = models.CharField(max_length=100, blank=True)
     comment = models.CharField(max_length=255, blank=True)
 
     rate = models.IntegerField(default=1)
     ip = models.CharField(max_length=50, blank=True)
-    status = models.CharField(choices=STATUS, max_length=10, default='Nova')
+    status = models.IntegerField(choices=StatusChoice.choices, default=StatusChoice.NEW)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

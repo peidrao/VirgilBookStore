a32c15c94d5c3f0b2b5ddfebcd9e210cfa77bb28
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-24 16:04:28 -0300

    remove book

--- book/urls.py
+++ book/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
     path('book/<str:slug>/', views.BookByGenreView.as_view(), name='books_by_genre'),
     path('books/', views.ManagerBoksView.as_view(), name='books'),
+    path('book/remove/<int:pk>', views.ManageRemoveBook.as_view(), name='remove'),
 ]

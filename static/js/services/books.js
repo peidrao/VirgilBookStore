8a7e9de360f2089a82e8c750e54ff2b8103a4227
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-24 16:04:41 -0300

    remove book js

--- static/js/services/books.js
+++ static/js/services/books.js
@@ -1,5 +1,51 @@
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === name + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
 $(".remove-book").on("click", (e) => {
   e.preventDefault();
   let book_id = e.currentTarget.id;
-  console.log(book_id);
+
+  headers = {
+    "X-CSRFToken": getCookie("csrftoken"),
+  };
+
+  console.log(headers);
+  Swal.fire({
+    title: "Tem certeza que deseja remover este livro?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Sim",
+    cancelButtonText: "Nao",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      $.ajax({
+        type: "DELETE",
+        dataType: "json",
+        url: `/book/remove/${book_id}`,
+        headers: headers,
+        success: function (response) {
+          Swal.fire("Removido!", "Livro removido", "success");
+          location.reload();
+        },
+
+        error: function (err) {
+          console.log(err.responseJSON);
+        },
+      });
+    }
+  });
 });

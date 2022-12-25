79660fb516e9cb5b2380bb2f6e858e2344157c00
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-25 20:19:14 -0300

    remove unused functions

--- user/urls.py
+++ user/urls.py
@@ -6,15 +6,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('', views.index, name='user_index'),
-    path('comments/', views.user_comments, name='user_comments'),
-    path('deletecomment/<int:id>', views.user_deletecomment,
-         name='user_deletecomment'),
-    path('orders/', views.user_orders, name='user_orders'),
-    path('request/', views.user_request, name='user_request'),
-    path('order_book/', views.user_order_book, name='user_order_book'),
-    path('signup/', views.signup_form, name='signup'),
-    path('login/', views.login, name='login'),
-    path('profile/', views.index, name='profile'),
     
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.DashBoardProfile.as_view(), name='dashboard'),
@@ -23,4 +14,5 @@ urlpatterns = [
    
     path('update_password/', views.UpdateProfileView.as_view(), name='update_password'),
     path('update_password_service/', views.UpdatePasswordService.as_view(), name='update_password_service'),
+    path('accounts/', views.AccountsListView.as_view(), name='accounts'),
 ]
--- user/views.py
+++ user/views.py
@@ -10,7 +10,7 @@ from django.views import generic
 from django.contrib.auth.views import LogoutView
 from django.views.generic import RedirectView
 
-from virgilbookstore.permissions import LoginRequiredPermission
+from virgilbookstore.permissions import AdministratorPermission, LoginRequiredPermission
 from .models import Profile, ProfileNewsletter, ProfileOffer
 from .forms import LoginAuthenticationForm, LoginForm, SignUpForm
 from book.models import Comment
@@ -90,8 +90,16 @@ class AddProfileNewsletterView(views.APIView):
 
 
 class DashBoardProfile(LoginRequiredPermission, generic.TemplateView):
-    queryset = Profile.objects.all()
     template_name = 'pages/dashboard.html'
+
+
+class AccountsListView(AdministratorPermission, generic.ListView):
+    queryset = Profile.objects.all()
+    template_name = 'user/accounts_table.html'
+
+    def get(self, request):
+        context = {'profiles': self.queryset.all()}
+        return render(request, self.template_name, context)
 
 
 class UpdateProfileView(generic.TemplateView):
@@ -112,8 +120,6 @@ class UpdatePasswordService(views.APIView):
             profile.save()
             return Response({'message': 'Senha alterada com sucesso'}, status=status.HTTP_200_OK)
         return Response({'message': 'Houve problema a salvar sua senha'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        
 
 
 class LogoutView(RedirectView):
@@ -125,45 +131,3 @@ class LogoutView(RedirectView):
         if self.request.user.is_authenticated:
             logout(self.request)
         return super(LogoutView, self).get_redirect_url(*args, **kwargs)
-
-
-@login_required(login_url='/login')
-def user_comments(request):
-    context = {
-        'comments': Comment.objects.filter(user=request.user)
-    }
-
-    return render(request, 'user/user_comments.html', context)
-
-
-@login_required(login_url='/login')
-def user_deletecomment(request, id):
-    Comment.objects.filter(profile=request.user, id=id).delete()
-    messages.success(request, 'Comentário deleteado!')
-    return HttpResponseRedirect(reverse('user:user_comments'))
-
-
-@login_required(login_url='/login')
-def user_orders(request):
-    context = {
-        'order': Order.objects.filter(profile=request.user),
-    }
-
-    return render(request, 'user/user_orders.html', context)
-
-
-@login_required(login_url='/login')
-def user_request(request):
-    context = {
-        'order': Order.objects.filter(profile=request.user),
-    }
-
-    return render(request, 'user/user_request.html', context)
-
-
-@login_required(login_url='/login')
-def user_order_book(request):
-    context = {
-        'order': Order.objects.filter(profile=request.user),
-    }
-    return render(request, 'user/user_order_books.html', context)

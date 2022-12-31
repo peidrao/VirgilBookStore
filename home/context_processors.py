9ee8d86b4eeda0b1f85b258eab84faa30edd1aa2
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-30 22:12:21 -0300

    context wishlist

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,5 +1,6 @@
 from book.models import Genre, Writer
 from home.models import Banner
+from market.models import WishList
 
 
 def get_genres(request):
@@ -16,3 +17,11 @@ def get_banners(request):
 def get_writers(request):
     writers = Writer.objects.all()
     return {"writers": writers}
+
+
+def get_wishlist_count(request):
+    return {
+        "wishlist": WishList.objects.filter(
+            profile=request.user, is_active=True
+        ).count()
+    }

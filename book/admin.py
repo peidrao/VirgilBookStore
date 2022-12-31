49f29fac7b9823c8dffbf86a81c25e43e5887ade
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-31 18:18:59 -0300

    add pyproject

--- book/admin.py
+++ book/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Book, Genre, Images, Comment, Writer
+from .models import Book, Genre, Comment, Writer
 
 
 class WriterAdmin(admin.ModelAdmin):

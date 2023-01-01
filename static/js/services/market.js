c385d46a32b16f987cad8119528cc562b2d928ee
Pedro Fonseca <contatopedrorn@gmail.com>
2022-12-31 23:27:20 -0300

    add and remove cart

--- static/js/services/market.js
+++ static/js/services/market.js
@@ -33,3 +33,60 @@ $(".remove-wishlist").on("click", (e) => {
     }
   });
 });
+
+$(".remove-cart").on("click", (e) => {
+  e.preventDefault();
+  let cart = e.currentTarget.id;
+  headers = {
+    "X-CSRFToken": getCookie("csrftoken"),
+  };
+
+  Swal.fire({
+    title: "Are you sure you want to remove this book",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes",
+    cancelButtonText: "Not",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      $.ajax({
+        type: "DELETE",
+        dataType: "json",
+        url: `/cart/${cart}`,
+        headers: headers,
+        success: function (response) {
+          console.log(response);
+          Swal.fire("Success!", response, "success");
+          window.location.reload();
+        },
+        error: function (err) {
+          console.log(err.responseJSON);
+        },
+      });
+    }
+  });
+});
+
+$(".btn-plus").click(function () {
+  let id = this.id;
+  headers = {
+    "X-CSRFToken": getCookie("csrftoken"),
+  };
+
+  $.ajax({
+    type: "PUT",
+    dataType: "json",
+    url: `/cart/${id}`,
+    data: { action: "plus" },
+    headers: headers,
+    success: function (response) {
+      $(".total-price").text(response.total);
+      $(`#book-total-price-${id}`).text(response.price);
+    },
+    error: function (err) {
+      console.log(err.responseJSON);
+    },
+  });
+});

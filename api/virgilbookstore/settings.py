983154d8a19fdf55b084aa2e3358d1a126b092f2
🥶 Pedro Fonseca <contatopedrorn@gmail.com>
2024-01-14 20:20:24 -0300

    remove STATICFILES_DIRS

--- api/virgilbookstore/settings.py
+++ api/virgilbookstore/settings.py
@@ -103,10 +103,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.Profile"

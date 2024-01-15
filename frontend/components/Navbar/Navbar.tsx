71ae7b56a7c5b5d4bff2714ad57a88f3c3768ef5
🥶 Pedro Fonseca <contatopedrorn@gmail.com>
2024-01-15 11:12:14 -0300

    custom navbar

--- frontend/components/Navbar/Navbar.tsx
+++ frontend/components/Navbar/Navbar.tsx
@@ -1,17 +1,39 @@
+"use client";
+
+import useNavigation from "@/hook/use-navigation";
+
 const Navbar = () => {
+  const { isAuthor, isCategory, isLogin } = useNavigation();
+
   return (
-    <main className="bg-cyan-300 p-4">
+    <main className="bg-slate-700">
       <div className="flex justify-between">
         <div className="flex">
-          <div className="bg-red-400">VirgilBookStore</div>
-          <div className="flex">
-            <span className="px-4">Gênero</span>
-            <span className="px-4">Autores</span>
+          <div className="bg-red-400 p-4 text-white">
+          VirgilBook<span className="uppercase font-bold">Store</span>
+
+          </div>
+          <div className="flex p-4 text-slate-400 justify-center uppercase font-semibold">
+            <a className={isCategory ? "text-white" : "pr-4 hover:text-white"}>
+              Categorias
+            </a>
+            <a className={isAuthor ? "text-white" : "pr-4 hover:text-white"}>
+              Autores
+            </a>
           </div>
         </div>
 
-        <div>
-            <span>Login</span>
+        <div className="flex items-center">
+          <a
+            href="/login"
+            className={
+              isLogin
+                ? "p-4 uppercase font-semibold text-white"
+                : "text-slate-400 p-4 uppercase font-semibold hover:text-white"
+            }
+          >
+            Login
+          </a>
         </div>
       </div>
     </main>

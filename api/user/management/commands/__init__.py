f56570d35242113f2509988f2d5b969caf31db8e
Pedro Fonseca <contatopedrorn@gmail.com>
2025-12-27 09:50:25 -0300

    🚫 refactor: remove order and user modules

--- api/user/management/commands/__init__.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-
-from user.models import Profile
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        if not Profile.objects.filter(email="admin@email.com").exists():
-            Profile.objects.create_superuser("admin", "admin@email.com", "admin")

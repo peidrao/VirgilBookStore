231efd319b9f57415f916cd7b623e451d8340d6a
Pedro Fonseca <contatopedrorn@gmail.com>
2025-12-27 09:49:40 -0300

    🗑️ refactor: remove unused home API components

--- api/home/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-
-class BookHomeSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField()
-    price = serializers.FloatField()
-    image = serializers.ImageField()

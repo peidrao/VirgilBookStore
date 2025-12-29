02b6d21af6041a39badb42c11620dcd362d7e728
Pedro Fonseca <contatopedrorn@gmail.com>
2025-12-29 12:54:16 -0300

    feat: ✨ add sidebar navigation and update layout for user profile and main pages

--- virgilbookstore/settings.py
+++ virgilbookstore/settings.py
@@ -102,6 +102,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- virgilbookstore/urls.py
+++ virgilbookstore/urls.py
@@ -15,3 +15,7 @@ urlpatterns = [
     path("order/", include("order.urls")),
     path("shopcart/", OrderViews.shopcart, name="shopcart"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

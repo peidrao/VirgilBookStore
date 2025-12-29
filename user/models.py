8e15bf9bcdee92c0b0cc671c9a4f83e0dec66d19
Pedro Fonseca <contatopedrorn@gmail.com>
2025-12-29 11:55:08 -0300

    feat: ✨ remove unused fields from Profile model and update email field constraints

--- user/models.py
+++ user/models.py
@@ -6,10 +6,7 @@ from django.utils import timezone
 class Profile(AbstractUser):
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, blank=True)
-    zip_code = models.CharField(max_length=10, blank=True)
-    address = models.CharField(max_length=150, blank=True)
-    city = models.CharField(max_length=50, blank=True)
-    state = models.CharField(max_length=50, blank=True)
+
     created_at = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
@@ -17,9 +14,6 @@ class Profile(AbstractUser):
 
     def __str__(self):
         return self.username
-
-    def user_name(self):
-        return self.first_name + " " + self.last_name + " [" + self.username + "] "
 
 
 class ProfileOffer(models.Model):

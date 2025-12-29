305c92898d5c1652aa9abc51e55eb39a1fe7268f
Pedro Fonseca <contatopedrorn@gmail.com>
2025-12-29 11:55:18 -0300

    feat: ✨ update ProfileAdmin to display only phone number in admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Profile
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ["user_name", "address", "phone", "city"]
+    list_display = ["phone"]
 
 
 admin.site.register(Profile, ProfileAdmin)

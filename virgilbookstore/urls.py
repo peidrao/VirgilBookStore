f1cb9ce6be1bb322522ac23595fdd15f4c78e9a4
Pedro Fonseca <contatopedrorn@gmail.com>
2025-12-30 15:53:41 -0300

    feat: ✨ remove unused shopcart URL to streamline routing

--- virgilbookstore/urls.py
+++ virgilbookstore/urls.py
@@ -3,8 +3,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from order import views as OrderViews
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -13,7 +11,6 @@ urlpatterns = [
     path("", include("book.urls")),
     path("", include("market.urls")),
     path("order/", include("order.urls")),
-    path("shopcart/", OrderViews.shopcart, name="shopcart"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
